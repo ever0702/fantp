7bbedfd6f1c08eff012d049d348f335c4194a43f
BlackAsync <ever0702@gmail.com>
2016-08-07 23:17:10 -0400

    add

---
+++ js/pickCity/PickCityForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import Card from '../commonComponents/Card'
+
+
+@connect()
+export default class PickCityForm extends React.Component {
+	constructor(props){
+		super(props)
+	}
+
+	render() {
+		
+	}
+}
